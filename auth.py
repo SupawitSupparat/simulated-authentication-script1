7590f13b2ed5c3a45628fa4c8abe8e46c3ba180f
Supawit Supparat <Supawit.sup@ku.th>
2018-06-11 17:12:18 +0700

    Finish CLI fire python

--- auth.py
+++ auth.py
@@ -3,49 +3,87 @@ from __future__ import print_function
 from __future__ import division
 from pyrad.client import Client
 from pyrad.dictionary import Dictionary
+from concurrent.futures import ThreadPoolExecutor
+from Queue import Queue
+from multiprocessing import Pool
+import threading
+import random
 import socket
 import sys
+import time
 import pyrad.packet
 import mysql.connector
 import time
+import fire
+import concurrent.futures
+
 start_time = time.time()
-success = 0
-denied = 0
-session = 0
-cnx = mysql.connector.connect(user='admin', 
-                              password='admin',
-                              host='192.168.56.201',
-                              database='radius')
 
-cursor = cnx.cursor()
-query = ("SELECT Username,Password FROM test")
-cursor.execute(query)
-db = cursor.fetchall()
+class Script(object):
+    success = 0
+    denied = 0
+    session = 0
 
-for row in db :
-    if time.time() - start_time < 1 :
-        session += 1
-        srv = Client(server="192.168.56.201", secret=b"testing123", dict=Dictionary("dictionary"))
-        req = srv.CreateAuthPacket(code=pyrad.packet.AccessRequest, User_Name=row[0])
-        req["User-Password"] = req.PwCrypt(row[1])
+    def __init__(self, username, password,request,server,secret,timeout,auth_port = 1812,acct_port = 1813):
+      self.username = username
+      self.password = password
+      self.acct_port = auth_port
+      self.auth_port = auth_port
+      self.request = request
+      self.server = server
+      self.secret = secret
+      self.timeout = timeout
+
+    def task(self):
+          srv = Client(server=self.server, secret=self.secret, dict=Dictionary("dictionary"))
+          req = srv.CreateAuthPacket(code=pyrad.packet.AccessRequest, User_Name=self.username)
+          req["User-Password"] = req.PwCrypt(self.password)
+          self.authandacct(srv,req)
     
+    def login(self):
+        self.task()
+       
+
+
+    def auth(self,srv,req):
+       
+        self.session += 1    
         try:
             reply = srv.SendPacket(req)
         except pyrad.client.Timeout:
             sys.exit(1)
-        except socket.error as error:
+        except socket.error :
             sys.exit(1)
         if reply.code == pyrad.packet.AccessAccept:
             print("Access accepted")
-            success += 1
+            self.success += 1
         else:
             print("Access denied")
-            denied +=1
+            self.denied +=1
 
-cursor.close()
-cnx.close()
-print("Sessions transmitted: : ", session)
-print("Responses received : ", success)
-rate = success/session*100
-print("Responses received rate: ", rate)
-print("Timeout %.3s seconds " % (time.time() - start_time))
+        print ("Start ",time.strftime("%d/%m/%Y"),time.strftime("%H:%M:%S"))
+        print("Sessions transmitted: : ", self.session)
+        print("Responses received : ", self.success)
+        rate = self.success/self.session*100
+        print("Responses received rate: ", rate)
+        print("Timeout %s seconds " % (time.time() - start_time))
+      
+    def acct(self,srv, req):
+     print("Sending accounting start packet")
+     req["Acct-Status-Type"] = "Start"
+     print("Sending accounting stop packet")
+     req["Acct-Status-Type"] = "Stop"
+     req["Acct-Input-Octets"] = random.randrange(2**10, 2**30)
+     req["Acct-Output-Octets"] = random.randrange(2**10, 2**30)
+     req["Acct-Session-Time"] = random.randrange(120, 3600)
+     req["Acct-Terminate-Cause"] = random.choice(["User-Request", "Idle-Timeout"])
+
+    
+    def authandacct(self,srv,req):
+        if self.request == 'auth':
+            self.auth(srv,req) 
+        else:
+            self.acct(srv,req)
+
+if __name__ == '__main__':
+   fire.Fire(Script)
