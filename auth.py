336943fdc3d180c5474e2aed2aaa4f346fa0adc0
Supawit Supparat <Supawit.sup@ku.th>
2018-06-12 15:37:00 +0700

    thread

--- auth.py
+++ auth.py
@@ -30,7 +30,7 @@ class Script(object):
     session = 0
     concurrent = 300
     timeout = 60.0
-
+    threads = []
     def __init__(self, username, password,request,server,secret,timeout,auth_port = 1812,acct_port = 1813):
       self.username = username
       self.password = password
@@ -49,60 +49,42 @@ class Script(object):
           self.authandacct(srv,req)
         
     def login(self):
-      q = Queue()
-      process_one = Process(target=self.task())
-      process_two = Process(target=self.task())
-      process_three = Process(target=self.task())
-      process_four = Process(target=self.task())
-      process_five = Process(target=self.task())
-      process_one.start()
-      process_two.start()
-      process_three.start()
-      process_four.start()
-      process_five.start()
-      q.close()
-      q.join_thread()
+            for i in range (5):
+             t = threading.Thread(target = self.task())
+             self.threads.append(t)
+             t.start()
+            
+            print ("Start ",time.strftime("%d/%m/%Y"),time.strftime("%H:%M:%S"))
+            print("Sessions transmitted: : ", self.session)
+            print("Responses received : ", self.success)
+            print("Responses received rate: ", self.success/self.session*100)
+            print("Timeout %s seconds " % (time.time() - start_time))
 
-      process_one.join()
-      process_two.join()
-      process_three.join()
-      process_four.join()
-      process_five.join()
 
-      
     def auth(self,srv,req):
         self.session += 1    
         reply = srv.SendPacket(req)
         if reply.code == pyrad.packet.AccessAccept:
-            print("Access accepted")
             self.success += 1
         else:
-            print("Access denied")
             self.denied +=1
-
-        print ("Start ",time.strftime("%d/%m/%Y"),time.strftime("%H:%M:%S"))
-        print("Sessions transmitted: : ", self.session)
-        print("Responses received : ", self.success)
-        rate = self.success/self.session*100
-        print("Responses received rate: ", self.success/self.session*100)
-        print("Timeout %s seconds " % (time.time() - start_time))
       
-    def acct(self,srv, req):
-     print("Sending accounting start packet")
-     req["Acct-Status-Type"] = "Start"
-     print("Sending accounting stop packet")
-     req["Acct-Status-Type"] = "Stop"
-     req["Acct-Input-Octets"] = random.randrange(2**10, 2**30)
-     req["Acct-Output-Octets"] = random.randrange(2**10, 2**30)
-     req["Acct-Session-Time"] = random.randrange(120, 3600)
-     req["Acct-Terminate-Cause"] = random.choice(["User-Request", "Idle-Timeout"])
+    # def acct(self,srv, req):
+    #  print("Sending accounting start packet")
+    #  req["Acct-Status-Type"] = "Start"
+    #  print("Sending accounting stop packet")
+    #  req["Acct-Status-Type"] = "Stop"
+    #  req["Acct-Input-Octets"] = random.randrange(2**10, 2**30)
+    #  req["Acct-Output-Octets"] = random.randrange(2**10, 2**30)
+    #  req["Acct-Session-Time"] = random.randrange(120, 3600)
+    #  req["Acct-Terminate-Cause"] = random.choice(["User-Request", "Idle-Timeout"])
 
     
     def authandacct(self,srv,req):
         if self.request == 'auth':
             self.auth(srv,req) 
-        else:
-            self.acct(srv,req)
+        # else:
+        #     self.acct(srv,req)
 
 
 if __name__ == '__main__':
